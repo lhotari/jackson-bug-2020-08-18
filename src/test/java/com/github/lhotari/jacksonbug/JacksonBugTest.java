6d0838ebe60ef7a363ccb330bd1b12be01b08a62
Lari Hotari <lari@hotari.net>
2020-08-18 14:16:45 +0300

    Remove Lombok and ensure Java 8 compatibility

--- src/test/java/com/github/lhotari/jacksonbug/JacksonBugTest.java
+++ src/test/java/com/github/lhotari/jacksonbug/JacksonBugTest.java
@@ -8,20 +8,25 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.cloudevents.CloudEvent;
 import io.cloudevents.v1.CloudEventBuilder;
 import io.cloudevents.v1.CloudEventImpl;
-import lombok.*;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
+import java.util.Collections;
 import java.util.List;
 
 public class JacksonBugTest {
-    @Value
-    @Builder
-    @AllArgsConstructor(onConstructor = @__(@JsonCreator))
     @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-    static class MyValue {
-        @Singular
-        List<CloudEvent<?, ?>> events;
+    static final class MyValue {
+        private final List<CloudEvent<?, ?>> events;
+
+        @JsonCreator
+        public MyValue(List<CloudEvent<?, ?>> events) {
+            this.events = events;
+        }
+
+        public List<CloudEvent<?, ?>> getEvents() {
+            return this.events;
+        }
     }
 
     // this test passes with Jackson 2.11.1, but fails with Jackson 2.11.2
@@ -34,7 +39,7 @@ public class JacksonBugTest {
                 .withSource(URI.create("urn:source"))
                 .withData("Hello")
                 .build();
-        MyValue val = MyValue.builder().event(cloudEvent).build();
+        MyValue val = new MyValue(Collections.singletonList(cloudEvent));
         // fails with com.fasterxml.jackson.databind.JsonMappingException: Strange Map type java.util.Map: cannot determine type parameters (through reference chain: com.github.lhotari.jacksonbug.JacksonTest$MyValue["events"]->java.util.Collections$SingletonList[0]->io.cloudevents.v1.CloudEventImpl["attributes"])
         objectMapper.writeValueAsString(val);
     }
